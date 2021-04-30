90139805372d0d9df84a9ed307353f0e209724b7
Gamita-Mejia <83315052+Gamita-Mejia@users.noreply.github.com>
2021-04-29 20:14:51 -0500

    Delete analiticos directory

--- analiticos/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-
-from analiticos.models import Archivo, Proceso
-# Register your models here.
-
-admin.site.register(Archivo)
-admin.site.register(Proceso)
--- analiticos/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class AnaliticosConfig(AppConfig):
-    name = 'analiticos'
